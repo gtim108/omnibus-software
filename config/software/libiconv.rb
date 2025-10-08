1574f1e851ab60d8a599b52f7874b3ea64b66991
Daniel Hougaard <daniel@infisical.com>
2025-10-08 18:18:57 +0400

    Update libiconv.rb

--- config/software/libiconv.rb
+++ config/software/libiconv.rb
@@ -27,12 +27,15 @@ skip_transitive_dependency_licensing true
 
 dependency "config_guess"
 
+
 # versions_list: https://ftp.gnu.org/pub/gnu/libiconv/ filter=*.tar.gz
+# custom repo for libiconv: https://artifacts-infisical-core.infisical.com/build-dependencies/libiconv-gnu
+
 version("1.17") { source sha256: "8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313" }
 version("1.16") { source sha256: "e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04" }
 version("1.15") { source sha256: "ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178" }
 
-source url: "https://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{version}.tar.gz"
+source url: "https://artifacts-infisical-core.infisical.com/build-dependencies/libiconv-gnu/libiconv-#{version}.tar.gz"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
                 authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
 
